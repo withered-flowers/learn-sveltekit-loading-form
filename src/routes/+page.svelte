3a8ae02e2f6cb035e8582bbe638f51ebb47d4e9e
Withered Flowers <withered-flowers@users.noreply.github.com>
2022-12-17 22:04:21 +0700

    feat: adding footer

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -27,6 +27,7 @@
 	<div class="mb-4">
 		<h1 class="text-3xl">Welcome to project loader</h1>
 		<h2 class="text-2xl">Simple project to learn how to use form loading</h2>
+		<h3 class="text-xl">(Progressive Enhancement + Logic)</h3>
 	</div>
 
 	<!-- We will use:enchance to use the Progressive enhancement of the form -->
@@ -38,7 +39,7 @@
 		<h1 class="text-base">Form that is used to POST a data</h1>
 		<input
 			type="password"
-			class="p-2 rounded text-slate-700"
+			class="p-2 rounded text-slate-700 w-96 text-center"
 			name="secret"
 			id="secret"
 			placeholder="Secret Text"
@@ -59,7 +60,7 @@
 
 		<!-- See the return from +page.server.ts has object with success and secretMessage props -->
 		<!-- We will use that props value here -->
-		{#if form?.success}
+		{#if form?.success && !loading}
 			<div>
 				<p>This is the secret encoded in base64:</p>
 				<p><strong class="font-semibold animate-pulse">{form?.secretMessage}</strong></p>
@@ -69,14 +70,37 @@
 
 	<div>
 		<h3 class="text-xl">
-			See this <a href="https://github.com" class="underline text-blue-500 hover:text-blue-700"
-				>Github</a
+			See this <a
+				href="https://github.com/withered-flowers/learn-sveltekit-loading-form"
+				class="underline text-blue-500 hover:text-blue-700">Github</a
 			> for more info about the code
 		</h3>
-		<h3 class="text-xl">File to analyze:</h3>
+		<h3 class="text-xl mt-4">File to analyze:</h3>
 		<ul class="text-xl">
-			<li class="font-mono font-semibold text-sm">+page.server.ts</li>
-			<li class="font-mono font-semibold text-sm">+page.svelte</li>
+			<li class="font-mono font-semibold text-sm">src/routes/+page.server.ts</li>
+			<li class="font-mono font-semibold text-sm">src/routes/+page.svelte</li>
 		</ul>
 	</div>
+
+	<footer class="py-8">
+		<div>&copy; withered-flowers - 2022</div>
+		<div class="flex items-center justify-center gap-1">
+			<span>Made with </span>
+			<span>
+				<svg width="24" height="24" viewBox="0 0 24 24" class="text-red-400">
+					<path
+						fill="currentColor"
+						d="m12 21.35l-1.45-1.32C5.4 15.36 2 12.27 2 8.5C2 5.41 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.08C13.09 3.81 14.76 3 16.5 3C19.58 3 22 5.41 22 8.5c0 3.77-3.4 6.86-8.55 11.53L12 21.35Z"
+					/>
+				</svg>
+			</span>
+			<span>
+				using <strong class="font-semibold">
+					<a href="https://kit.svelte.dev/" class="underline text-blue-500 hover:text-blue-700"
+						>SvelteKit</a
+					></strong
+				></span
+			>
+		</div>
+	</footer>
 </section>
